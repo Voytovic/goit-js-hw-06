676b30869c21aff4e22994bbbd128ee927b5fe75
Voytovic <voytovic@gmail.com>
2023-12-24 22:26:11 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -14,10 +14,7 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this.#items.splice(index, 1);
-    }
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 
